24ff64154217ac20b0baec768bf6e32d8cb36361
vitebo <vitebo@hotmail.com>
2018-03-22 23:02:17 -0300

    refactoring webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,9 @@
 const mix = require('laravel-mix');
-const nodeENV = process.env.NODE_ENV;
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
 
-if(nodeENV === 'development') {
-   mix.js('resources/assets/js/app.js', 'public/js')
-      .sass('resources/assets/sass/app.scss', 'public/css')
-      .mix.browserSync('localhost:8000');
-} else if(nodeENV === 'production') {
-  mix.js('resources/assets/js/app.js', 'public/js')
-     .sass('resources/assets/sass/app.scss', 'public/css')
-     .version();
-}
+if(mix.inProduction())
+  mix.version();
+else
+  mix.browserSync('localhost:8000');
