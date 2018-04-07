a27bb8fe2b79b1ece89928984ec797446c197b50
vitebo <vitebo@hotmail.com>
2018-04-07 12:37:20 -0300

    creating custom svg

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,10 @@ import Vue from 'vue'
 import ExampleComponent from './components/ExampleComponent'
 import CustomInput from './components/CustomInput'
 import CustomCheckbox from './components/CustomCheckbox'
+import CustomSvg from './components/CustomSvg'
+import VueSimpleSVG from 'vue-simple-svg'
 
+Vue.use(VueSimpleSVG)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -13,6 +16,7 @@ new Vue({
   components: {
     CustomInput,
     ExampleComponent,
-    CustomCheckbox
+    CustomCheckbox,
+    CustomSvg
   }
 })
---
+++ resources/assets/js/components/CustomSvg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="cssClass" v-show="isReady">
+    <simple-svg
+      :filepath="filepath"
+      @ready="onSvgReady()">
+    </simple-svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "custom-svg",
+    props: [
+      'filepath',
+      'css-class',
+    ],
+    data () {
+      return {
+        isReady: false
+      }
+    },
+    methods: {
+      onSvgReady () {
+        this.isReady = true
+      }
+    }
+  }
+</script>
