03dc4c2b15e70a2aaf26a4d069206d57aacbe11a
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2018-04-07 22:26:47 -0300

    template

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+@push('HeaderScripts')
+<!-- Header Personalizado [Modelo] -->
+
+<!-- /Header Personalizado [Modelo] -->
+@endpush
+
+@section('title', '')
+
+@section('content')
+    <!-- Conteudo Principal [Modelo] -->
+
+    <!-- /Conteudo Principal [Modelo] -->
+@endsection
+
+@push('FooterScripts')
+<!-- Footer Personalizado [Modelo] -->
+
+<!-- /Footer Personalizado [Modelo] -->
+@endpush
