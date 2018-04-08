141f0a79457ed2b7eda57a93487ab7a7b4e44fdb
vitebo <vitebo@hotmail.com>
2018-04-08 13:41:27 -0300

    configuring webpack for multiple outputs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,42 @@
-const mix = require('laravel-mix')
+const MIX = require('laravel-mix')
 
-mix.js('resources/assets/js/*.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
+const ASSETS_PATH = 'resources/assets/'
+const FILES = [
+  `auth`,
+  `admin`
+]
 
-if (mix.inProduction()) { mix.version() } else {
-  mix.browserSync('localhost:8000')
+config()
+
+function config () {
+  configState()
+  configJs()
+  configSass()
+}
+
+function configState () {
+  MIX.inProduction() ? configInProduction() : configInDevelopment()
+}
+
+function configJs () {
+  FILES.forEach((file) => {
+    MIX.js(`${ASSETS_PATH}/js/${file}.js`, 'public/js')
+  })
+}
+
+function configSass () {
+  FILES.forEach((file) => {
+    MIX.sass(`${ASSETS_PATH}/sass/${file}.scss`, 'public/css')
+  })
+}
+
+function configInProduction () {
+  MIX.version()
+}
+
+function configInDevelopment () {
+  MIX
+    .browserSync('localhost:8000')
     .webpackConfig({
       devtool: 'source-map'
     })
