dac5800f539cfc2eb75c3c8ae1a9512e6bd110a9
vitebo <vitebo@hotmail.com>
2018-04-08 13:47:23 -0300

    creating remaining files

--- resources/assets/js/auth.js
+++ resources/assets/js/auth.js
@@ -1,11 +1,12 @@
-import './bootstrap'
-import './fontawesome-config'
 import Vue from 'vue'
 import ExampleComponent from './components/ExampleComponent'
 import CustomInput from './components/CustomInput'
 import CustomCheckbox from './components/CustomCheckbox'
 import CustomSvg from './components/CustomSvg'
 import VueSimpleSVG from 'vue-simple-svg'
+
+require('./config/bootstrap')
+require('./config/icons')
 
 Vue.use(VueSimpleSVG)
 Vue.config.productionTip = false
